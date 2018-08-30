9bcd818ca972ecdceb7862abd468de96079be2af
Aleksandr Klubovich <AVKlubovich@gmail.com>
2018-08-30 16:26:33 +0300

    finish spawn points

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+        PlayerController playerController = GetComponent<PlayerController>();
+        playerController.enabled = isLocalPlayer;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+}
