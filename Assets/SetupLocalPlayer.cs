a2b5e9522384c6e6f59a1f50a9d02e34c6d45a59
Aleksandr Klubovich <AVKlubovich@gmail.com>
2018-08-31 15:44:31 +0300

    add player name label

--- Assets/SetupLocalPlayer.cs
+++ Assets/SetupLocalPlayer.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
 
 public class SetupLocalPlayer : NetworkBehaviour
 {
+    public Text namePrefab;
+    public Text nameLabel;
+    public Transform namePos;
+
+    public string playerName = "player";
+
     // Use this for initialization
     void Start()
     {
@@ -18,10 +26,30 @@ public class SetupLocalPlayer : NetworkBehaviour
         {
             playerController.enabled = false;
         }
+
+        this._prepareName();
+    }
+
+    private void _prepareName()
+    {
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        this.nameLabel = Instantiate(this.namePrefab, Vector3.zero, Quaternion.identity) as Text;
+        this.nameLabel.transform.SetParent(canvas.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(this.namePos.position);
+        this.nameLabel.transform.position = nameLabelPos;
+    }
+    
+    void OnGUI()
+    {
+        if (isLocalPlayer)
+        {
+            playerName = GUI.TextField(new Rect(25, 15, 100, 25), playerName);
+            nameLabel.text = playerName;
+        }
     }
 }
