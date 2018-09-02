75cd147daf001e28f8075f176b8ea667e210b813
Aleksandr Klubovich <AVKlubovich@gmail.com>
2018-09-02 21:55:12 +0300

    bullet 3

--- Assets/FireController.cs
+++ Assets/FireController.cs
@@ -29,13 +29,26 @@ public class FireController : NetworkBehaviour
         }
     }
 
+    void CreateBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward * 50;
+        Destroy(bullet, 4);
+    }
+
+    [ClientRpc]
+    void RpcCreateBullet()
+    {
+        if (!isServer)
+        {
+            CreateBullet();
+        }
+    }
+
     [Command]
     void CmdShoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-        // bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.forward * 2000);
-        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward * 50;
-        NetworkServer.Spawn(bullet);
-        Destroy(bullet, 4);
+        CreateBullet();
+        RpcCreateBullet();
     }
 }
