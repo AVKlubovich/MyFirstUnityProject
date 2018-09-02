3178339e6ca60f6ef7f0f58e240106e62dca1ce3
Aleksandr Klubovich <AVKlubovich@gmail.com>
2018-09-02 21:43:10 +0300

    bullet 2

--- Assets/FireController.cs
+++ Assets/FireController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
 using UnityEngine;
 
-public class FireController : MonoBehaviour
+public class FireController : NetworkBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject bulletSpawn;
+
+
 
     // Use this for initialization
     void Start ()
@@ -15,11 +18,24 @@ public class FireController : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         if (Input.GetKeyDown("space"))
         {
-            GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-            bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.forward * 2000);
-            Destroy(bullet, 4);
+            CmdShoot();
         }
+    }
+
+    [Command]
+    void CmdShoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        // bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.forward * 2000);
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward * 50;
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 4);
     }
 }
