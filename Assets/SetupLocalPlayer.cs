c84e075898efb735053fa5fe7d701c0881a924e1
Aleksandr Klubovich <AVKlubovich@gmail.com>
2018-09-02 14:07:11 +0300

    add sync players name and color

--- Assets/SetupLocalPlayer.cs
+++ Assets/SetupLocalPlayer.cs
@@ -9,10 +9,28 @@ public class SetupLocalPlayer : NetworkBehaviour
     public Text namePrefab;
     public Text nameLabel;
     public Transform namePos;
-    string textboxName = "";
+    string textBoxName = "";
+
+    string colorBoxName = "";
 
     [SyncVar (hook = "OnChangeName")]
     public string playerName = "player";
+
+    [SyncVar (hook = "OnChangeColor")]
+    public string playerColor = "#ffffff";
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        Invoke("UpdateStates", 1);
+    }
+
+    void UpdateStates()
+    {
+        OnChangeName(playerName);
+        OnChangeColor(playerColor);
+    }
 
     // Use this for initialization
     void Start()
@@ -43,22 +61,59 @@ public class SetupLocalPlayer : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(this.namePos.position);
-        this.nameLabel.transform.position = nameLabelPos;
+        //determine if the object is inside the camera's viewing volume
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(this.transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 &&
+            screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        //if it is on screen draw its label attached to is name position
+        if (onScreen)
+        {
+            Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(this.namePos.position);
+            this.nameLabel.transform.position = nameLabelPos;
+        }
+        else //otherwise draw it WAY off the screen 
+        {
+            this.nameLabel.transform.position = new Vector3(-1000, -1000, 0);
+        }
     }
     
     void OnGUI()
     {
         if (isLocalPlayer)
         {
-            this.textboxName = GUI.TextField(new Rect(25, 15, 100, 25), this.textboxName);
+            this.textBoxName = GUI.TextField(new Rect(25, 15, 100, 25), this.textBoxName);
             if (GUI.Button(new Rect(130, 15, 55, 25), "Change"))
             {
-                CmdChangeName(this.textboxName);
-                this.textboxName = "";
-                GUI.TextField(new Rect(25, 15, 100, 25), this.textboxName);
+                CmdChangeName(this.textBoxName);
+                this.textBoxName = "";
+                GUI.TextField(new Rect(25, 15, 100, 25), this.textBoxName);
+            }
+
+            this.colorBoxName = GUI.TextField(new Rect(190, 15, 100, 25), this.colorBoxName);
+            if (GUI.Button(new Rect(295, 15, 55, 25), "Change"))
+            {
+                CmdChangeColor(this.colorBoxName);
+                this.colorBoxName = "";
+                GUI.TextField(new Rect(190, 15, 100, 25), this.colorBoxName);
             }
         }
+    }
+
+    Color ColorFromHex(string hex)
+    {
+        hex = hex.Replace("0x", "");
+        hex = hex.Replace("#", "");
+        byte a = 255;
+        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+
+        if (hex.Length == 8)
+        {
+            a = byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
+        }
+
+        return new Color32(r, g, b, a);
     }
 
     [Command]
@@ -72,5 +127,35 @@ public class SetupLocalPlayer : NetworkBehaviour
     {
         this.playerName = newName;
         this.nameLabel.text = this.playerName;
+    }
+
+    [Command]
+    public void CmdChangeColor(string newColor)
+    {
+        this.OnChangeColor(newColor);
+    }
+
+    public void OnChangeColor(string newColor)
+    {
+        this.playerColor = newColor;
+        Renderer[] rends = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer r in rends)
+        {
+            if (r.gameObject.name == "BODY" ||
+                r.gameObject.name == "LEFT_MIRROR" ||
+                r.gameObject.name == "RIGHT_MIRROR")
+            {
+                r.material.SetColor("_Color", ColorFromHex(this.playerColor));
+            }
+        }
+    }
+
+    public void OnDestroy()
+    {
+        if (this.nameLabel != null)
+        {
+           Destroy(this.nameLabel.gameObject);
+        }
     }
 }
